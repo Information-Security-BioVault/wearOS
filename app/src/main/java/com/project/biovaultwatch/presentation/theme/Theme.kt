62404dc36572162cab2db7b6f7180e359871877c
PIYUJIN <yjyj020201@gmail.com>
2024-04-06 16:20:59 +0900

    프로젝트 생성🎉

---
+++ app/src/main/java/com/project/biovaultwatch/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.project.biovaultwatch.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun BioVaultWatchTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
