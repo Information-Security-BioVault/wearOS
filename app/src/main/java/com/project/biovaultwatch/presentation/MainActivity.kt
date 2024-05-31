cae37313a119be94d83050ba1a408677f7cd954b
PIYUJIN <yjyj020201@gmail.com>
2024-05-31 12:23:46 +0900

    [Feat] #6 API 호출에 따른 화면 전환 기능 구현 및 API 연동

--- app/src/main/java/com/project/biovaultwatch/presentation/MainActivity.kt
+++ app/src/main/java/com/project/biovaultwatch/presentation/MainActivity.kt
@@ -12,23 +12,24 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.os.PowerManager
 import android.provider.Settings
 import android.util.Log
 import android.view.MotionEvent
-import android.widget.ImageButton
-import android.widget.ProgressBar
+import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -39,28 +40,36 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.CircularProgressIndicator
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
-import com.chaquo.python.PyException
-import com.chaquo.python.PyObject
-import com.chaquo.python.Python
-import com.chaquo.python.android.AndroidPlatform
+import com.google.android.material.snackbar.Snackbar
 import com.project.biovaultwatch.MyApplication
+import com.project.biovaultwatch.PreferenceUtil
 import com.project.biovaultwatch.R
+import com.project.biovaultwatch.api.ApiClient
+import com.project.biovaultwatch.api.model.request.AuthenticationRequestModel
+import com.project.biovaultwatch.api.model.request.EnrollRequestModel
+import com.project.biovaultwatch.api.model.response.AuthenticationResponseModel
+import com.project.biovaultwatch.api.model.response.EnrollResponseModel
 import com.project.biovaultwatch.presentation.theme.BioVaultWatchTheme
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainActivity : ComponentActivity() {
 
     var TAG = "심박수"
 
-    var heartRateList1 = mutableListOf<Float>()
-    var heartRateList2 = mutableListOf<Float>()
-    var heartRateList3 = mutableListOf<Float>()
-    var heartRateList4 = mutableListOf<Float>()
-    var heartRateList5 = mutableListOf<Float>()
+    var heartRateList = mutableListOf<Float>()
+
+    var deviceId = ""
+
+
+
 
 //    lateinit var client1: PyObject
 
@@ -70,24 +79,43 @@ class MainActivity : ComponentActivity() {
         override fun onSensorChanged(event: SensorEvent) {
             if (event.sensor.type == Sensor.TYPE_HEART_RATE) {
                 val heartRate = event.values[0]
-                if(heartRateList1.size < 100) {
-                    // 심박수 데이터를 처리합니다.
-                    heartRateList1.add(heartRate)
-                    MyApplication.progress += 0.01f
-                    Log.d(TAG, "heartRate list : ${heartRateList1}")
-                    Log.d(TAG, "heartRate : ${heartRate}")
-                    if(heartRateList1.size == 100) {
-                        Log.d(TAG, "heartRate list size 100")
-                        // 처음이 아닐 경우 서버 통신 (심박수 데이터 전송)
+                // 심박수 데이터를 처리합니다.
+                heartRateList.add(heartRate)
+                MyApplication.progress += 0.01f
+                Log.d(TAG, "heartRate list : ${heartRateList}")
+                Log.d(TAG, "heartRate : ${heartRate}")
+                Log.d(TAG, "heartRate : ${heartRateList.size}")
+                if(heartRateList.size == 100) {
+                    Log.d(TAG, "heartRate list size 100")
+                    // 처음이 아닐 경우 서버 통신 (심박수 데이터 전송)
+                    if(MyApplication.preferences.getString("isFirst", "true") == "false") {
+                        Log.d(TAG, "isFirst : ${MyApplication.preferences.getString("isFirst", "true")}")
+                        authenticate()
                     }
-
-                    if(heartRateList1.size == 500) {
-                        Log.d(TAG, "heartRate list size 500")
+                    else {
+                        Log.d(
+                            TAG,
+                            "isFirst : ${MyApplication.preferences.getString("isFirst", "true")}"
+                        )
                     }
                     // 파이썬 코드 실행을 위한 Chaquopy 플러그인 추가
                     /*  val encryptFunction = client1.callAttr("encrypt")
                         val encryption = encryptFunction.toString()
                         Log.d(TAG, "encryption value : ${encryption}") */
+                }
+                // 처음인 경우 서버 통신 (심박수 데이터 등록)
+                else if(heartRateList.size == 500) {
+                    Log.d(TAG, "heartRate list size 500")
+                    // 100개씩 나누어 5개의 리스트로 분할
+                    var chunkedLists = heartRateList.chunked(100)
+
+                    // 결과 출력
+                    chunkedLists.forEachIndexed { index, list ->
+                        Log.d(TAG, "List ${index + 1}: $list")
+                    }
+
+                    enroll(chunkedLists)
+                    heartRateList.clear()
                 }
     }
 }
@@ -108,6 +136,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             MainScreen()
         }
+
+        MyApplication.preferences = PreferenceUtil(applicationContext)
+
+        deviceId = Settings.Secure.getString(
+            contentResolver,
+            Settings.Secure.ANDROID_ID
+        )
+
+        Log.d(TAG,"deviceInformation 확인 : ${deviceId}")
+
 
         // 파이썬 코드 실행을 위한 Chaquopy 플러그인 추가
         /*         // Python 인터프리터 초기화
@@ -139,7 +177,7 @@ class MainActivity : ComponentActivity() {
         @Composable
         fun MainScreen() {
         BioVaultWatchTheme {
-            val navController = rememberNavController()
+            var navController = rememberNavController()
             NavHost(
                 navController = navController,
                 startDestination = "HomeScreen"
@@ -172,6 +210,7 @@ class MainActivity : ComponentActivity() {
                                 heartRateSensor?.also { sensor ->
                                     sensorManager.registerListener(heartRateListener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
                                 }
+
                                 navController.navigate("LoadingScreen")
                             },
                             enabled = true,
@@ -184,21 +223,25 @@ class MainActivity : ComponentActivity() {
                             )
                         }
 
-                        Image (
-                            painter = painterResource(id = R.drawable.setting),
+                        Button(
+                            onClick = {
+                                Log.d(TAG, "설정 버튼 클릭")
+
+                                navController.navigate("LoadingScreen")
+                            },
+                            enabled = true,
                             modifier = Modifier
-                                .padding(vertical = 100.dp)
-                                .pointerInteropFilter {
-                                    when (it.action) {
-                                        MotionEvent.ACTION_DOWN -> {
-                                            Log.d(TAG, "Pressed")
-                                        }
-                                        else -> false
-                                    }
-                                    true
-                                },
-                            contentDescription = "setting",
-                        )
+                                .padding(vertical = 70.dp)
+                                .fillMaxWidth(),
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = Color.Transparent,
+                                contentColor = Color.Gray),
+                        ) {
+                            Text(
+                                text = "설정"
+                            )
+                        }
+
                     }
                 }
                 composable("LoadingScreen"){
@@ -245,7 +288,104 @@ class MainActivity : ComponentActivity() {
                         )
                     }
                 }
+
+                composable("SettingScreen"){
+                    Box(
+                        //        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colors.background)
+                            .padding(10.dp),
+                    ) {
+                        TimeText()
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 40.dp),
+                            textAlign = TextAlign.Center,
+                            color = MaterialTheme.colors.primary,
+                            text = stringResource(R.string.setting_information, "Android"),
+                        )
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 40.dp),
+                            textAlign = TextAlign.Center,
+                            color = MaterialTheme.colors.primary,
+                            text = deviceId
+                        )
+                    }
+                }
             }
         }
+    }
+
+    fun enroll(list: List<List<Float>>) {
+        var apiClient = ApiClient(this)
+
+        var enrollmentData = EnrollRequestModel(list, deviceId)
+
+        sensorManager.unregisterListener(heartRateListener)
+
+        apiClient.apiService.enroll(enrollmentData)?.enqueue(object : Callback<EnrollResponseModel> {
+            override fun onResponse(call: Call<EnrollResponseModel>, response: Response<EnrollResponseModel>) {
+                if (response.isSuccessful) {
+                    // 정상적으로 통신이 성공된 경우
+                    var result: EnrollResponseModel? = response.body()
+                    Log.d("##", "onResponse 성공: " + result?.toString())
+                    Toast.makeText(this@MainActivity, "등록 성공", Toast.LENGTH_SHORT).show()
+                    MyApplication.preferences.setString("isFirst", "false")
+                } else {
+                    // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
+                    var result: EnrollResponseModel? = response.body()
+                    Log.d("##", "onResponse 실패")
+                    Log.d("##", "onResponse 실패: " + response.code())
+                    Log.d("##", "onResponse 실패: " + response.body())
+                    val errorBody = response.errorBody()?.string() // 에러 응답 데이터를 문자열로 얻음
+                    Log.d("##", "Error Response: $errorBody")
+
+                    Toast.makeText(this@MainActivity, "다시 시도해주세요.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<EnrollResponseModel>, t: Throwable) {
+                // 통신 실패
+                Log.d("##", "onFailure 에러: " + t.message.toString());
+            }
+        })
+    }
+
+    fun authenticate() {
+        var apiClient = ApiClient(this)
+
+        var authenticationData = AuthenticationRequestModel(deviceId, heartRateList)
+
+        apiClient.apiService.authenticate(authenticationData)?.enqueue(object : Callback<AuthenticationResponseModel> {
+            override fun onResponse(call: Call<AuthenticationResponseModel>, response: Response<AuthenticationResponseModel>) {
+                if (response.isSuccessful) {
+                    // 정상적으로 통신이 성공된 경우
+                    var result: AuthenticationResponseModel? = response.body()
+                    Log.d("##", "onResponse 성공: " + result?.toString())
+                    Toast.makeText(this@MainActivity, "인증 성공", Toast.LENGTH_SHORT).show()
+                    // 심박수 측정 센서 정지
+                    sensorManager.unregisterListener(heartRateListener)
+                } else {
+                    // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
+                    var result: AuthenticationResponseModel? = response.body()
+                    Log.d("##", "onResponse 실패")
+                    Log.d("##", "onResponse 실패: " + response.code())
+                    Log.d("##", "onResponse 실패: " + response.body())
+                    val errorBody = response.errorBody()?.string() // 에러 응답 데이터를 문자열로 얻음
+                    Log.d("##", "Error Response: $errorBody")
+
+                    Toast.makeText(this@MainActivity, "다시 시도해주세요.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<AuthenticationResponseModel>, t: Throwable) {
+                // 통신 실패
+                Log.d("##", "onFailure 에러: " + t.message.toString());
+            }
+        })
     }
 }
