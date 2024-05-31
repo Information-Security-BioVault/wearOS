a4eb2dc16948a4987fd30398bb57ee737f1a142b
PIYUJIN <yjyj020201@gmail.com>
2024-05-31 12:21:31 +0900

    [Feat] #6 서버 API 연동 초기 세팅

---
+++ app/src/main/java/com/project/biovaultwatch/api/ApiService.kt
@@ -0,0 +1,13 @@
+package com.project.biovaultwatch.api
+
+import com.project.biovaultwatch.api.model.request.AuthenticationRequestModel
+import com.project.biovaultwatch.api.model.request.EnrollRequestModel
+import com.project.biovaultwatch.api.model.response.AuthenticationResponseModel
+import com.project.biovaultwatch.api.model.response.EnrollResponseModel
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,14 +8,14 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven {url = uri("https://chaquo.com/maven/")}  // 파이썬 코드 실행을 위한 Chaquopy 플러그인 추가
+//        maven {url = uri("https://chaquo.com/maven/")}  // 파이썬 코드 실행을 위한 Chaquopy 플러그인 추가
         maven {url = uri("https://jitpack.io")}
         jcenter() // Warning: this repository is going to shut down soon
     }
 
     // 파이썬 코드 실행을 위한 Chaquopy 플러그인 추가
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("com.chaquo.python:gradle:15.0.1")
-    }
+//    dependencies {
+//        classpath("com.android.tools.build:gradle:7.0.4")
+//        classpath("com.chaquo.python:gradle:15.0.1")
+//    }
 }
